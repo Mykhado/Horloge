51f6f9a1ab16e9e246046f65976e34ffec78aeab
Mykhado <mikawestindies@gmail.com>
2022-09-20 15:53:21 +0200

    Projet finaliser

--- js/script.js
+++ js/script.js
@@ -4,65 +4,70 @@ const AIGUILLEMIN = document.getElementById("minute");
 const AIGUILLESEC = document.getElementById("second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
-let today = new Date();
-
 
 //Stocker l'heure , minute , seconde  dans des varaiables
-let  HR = today.getHours();
+let today = new Date();
+let HR = today.getHours();
 let MIN = today.getMinutes();
-let SEC= today.getSeconds();
-let heure;
-let minute;
-let seconde;
-let positionHR = HR*3600*heure;
-let positionMIN = MIN*60*minute;
-let positionSEC = SEC*seconde;
-
+let SEC = today.getSeconds();
+let heure = 360 / (12 * 3600);
+let minute = 360 / (60 * 60);
+let seconde = 360 / 60;
+let positionHR;
+let positionMIN;
+let positionSEC;
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
 // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-function hourDeg (){
-    heure = (360/(24*3600))
-   return heure;
+function hourDeg() {
+  heure = 360 / (12 * 3600);
+  return heure;
 }
 
 heure = hourDeg();
-console.log(heure);
 
-function minuteDeg (){ 
-    minute = 360/(60*60)
-    return minute;
- }
- minute = minuteDeg();
- console.log(minute);
-
-
- function secondeDeg (){ 
-    seconde = 360/60
-    return seconde;
- }
- seconde=secondeDeg();
- console.log(seconde);
-
-positionHR = HR*3600*heure;
-positionMIN = MIN*60*minute;
-positionSEC = SEC*seconde;
-
-
-   
-// Déplacer les aiguilles 
-function demarrerLaMontre() {
-AIGUILLEHR.style.transform= `rotate(${positionHR}deg)`;
-AIGUILLEMIN.style.transform= `rotate(${positionMIN}deg)`;
-AIGUILLESEC.style.transform=`rotate(${positionSEC}deg)`;
+function minuteDeg() {
+  minute = 360 / (60 * 60);
+  return minute;
 }
+minute = minuteDeg();
 
+function secondeDeg() {
+  seconde = 360 / 60;
+  return seconde;
+}
+seconde = secondeDeg();
 
+positionHR = HR * 3600 * heure;
+positionMIN = MIN * 60 * minute;
+positionSEC = SEC * seconde;
+
+AIGUILLEHR.style.transform += `rotate(${positionHR}deg)`;
+AIGUILLEMIN.style.transform += `rotate(${positionMIN}deg)`;
+AIGUILLESEC.style.transform += `rotate(${positionSEC}deg)`;
+// Déplacer les aiguilleslet
+
+function demarrerLaMontre() {
+  today = new Date();
+  HR = today.getHours();
+  MIN = today.getMinutes();
+  SEC = today.getSeconds();
+  heure = 360 / (12 * 3600);
+  minute = 360 / (60 * 60);
+  seconde = 360 / 60;
+  //  pour l'heure faire la somme de  (heure en seconde +minute en seconde +seconde) afin d'obtenir une donnée horaire precise a chaque seconde;
+  positionHR = (HR * 3600 + MIN * 60 + SEC) * heure;
+  //   idem que précédement pour un placement precis de l'aiguille des minutes.
+  positionMIN = (MIN * 60 + SEC) * minute;
+  positionSEC = SEC * seconde;
+  AIGUILLEHR.style.transform = `rotate(${positionHR}deg)`;
+  AIGUILLEMIN.style.transform = `rotate(${positionMIN}deg)`;
+  AIGUILLESEC.style.transform = `rotate(${positionSEC}deg)`;
+}
+const interval = setInterval(demarrerLaMontre, 1000);
 
 // Exercuter la fonction chaque second
 
-let interval = setInterval(demarrerLaMontre, 1000);
-
-console.log (positionHR)
-console.log (MIN*60*minute)
-console.log (SEC*seconde)
+// console.log(positionHR);
+// console.log(MIN * 60 * minute);
+// console.log(SEC * seconde);
